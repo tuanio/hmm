48b58634da04b6e5c45cc523818fc9421e7e6ef4
tuanio <nvatuan3@gmail.com>
2021-11-07 23:24:54 +0700

    change some viterbi code and add references to readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Description
 
 This is the respository doing about implementing Hidden Markov Model problems
+
+## References
+
+- https://web.stanford.edu/~jurafsky/slp3/A.pdf (Weather and Ice-cream problem)
--- viterbi.py
+++ viterbi.py
@@ -6,7 +6,7 @@ def viterbi(O):
     O -= 1
     T = len(O)
     delta = np.zeros(shape=(T, N))
-    psi = np.zeros_like(delta)
+    psi = np.zeros_like(delta, dtype='int')
 
     delta[0, :] = PI * B[:, O[0]]
     psi[0, :] = -1
@@ -16,22 +16,22 @@ def viterbi(O):
             delta_max = delta[t - 1, :] * A[:, j]
             delta[t, j] = np.max(delta_max) * B[j, O[t]]
             psi[t, j] = np.argmax(delta_max)
-    
-    P_star = np.max(delta[T - 1, :])
-    q = np.argmax(delta[T - 1, :]).astype('int')
 
-    psi = psi.astype('int')
-
-    best_state = [q]
+    P = np.max(delta[T - 1, :])
+    Q = np.zeros_like(O)
+    Q[-1] = np.argmax(delta[T - 1, :]).astype('int')
     for t in range(T - 2, -1, -1):
-        q = psi[t + 1, q]
-        best_state = [q] + best_state
-    return P_star, np.array(best_state)
+        Q[t] = psi[t + 1, Q[-1]]
+    return P, Q
 
+# observation
 O = np.array([1, 2, 1, 1, 2, 2, 2, 2, 3])
+
+# 
 P, best_state = viterbi(O)
 print(P, best_state)
 
+# test using hmmlearn library
 model_hmm = hmm.MultinomialHMM(n_components=2)
 model_hmm.startprob_ = PI
 model_hmm.transmat_ = A
